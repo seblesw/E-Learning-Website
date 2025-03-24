dd8d2ca30bd97af11e8facd606d547f7c9571c0d
Seble Sefineh <seblesoftware@gmail.com>
2025-03-24 17:11:09 +0300

    modify ui

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -54,7 +54,7 @@ const Home = () => {
 <header
   className="relative bg-white overflow-hidden shadow-lg h-screen"
   style={{
-    background: `linear-gradient(to right, #25A7E5 10%, #25A7E5 15%, rgba(0, 0, 0, 0.5) 30%, rgba(0, 0, 0, 0.5) 100%), 
+    background: `linear-gradient(to right, #25A7E5, #25A7E5 10%, rgba(0, 0, 0, 0.5) 30%, rgba(0, 0, 0, 0.5) 100%), 
                  url(${images[currentImageIndex]}) center/cover no-repeat`,
   }}
 >
@@ -80,7 +80,7 @@ const Home = () => {
     <div className="relative flex items-center justify-center p-4">
       <div className="rounded-lg px-6 py-3 text-center">
         <p className="text-white text-3xl font-light tracking-wide">{t("Available")}</p>
-        <div className="mt-1 px-6 py-2 text-5xl backdrop-blur-md border border-white text-white font-extrabold text-md rounded-md inline-block shadow-md">
+        <div className="mt-1 px-6 py-2 text-4xl backdrop-blur-md border border-white text-white font-extrabold text-md rounded-md inline-block shadow-md">
           <button>{t("Every Where")}</button>
         </div>
       </div>
@@ -88,10 +88,10 @@ const Home = () => {
   </div>
 </header>
 
-<section id="services" className=" py-60 bg-white" data-aos="fade-up ">
+<section id="services" className=" py-40  bg-cyan-50" data-aos="fade-up ">
  <div className="w-full">
   <div className="text-center  w-full">
-  <h2 className="text-3xl font-bold text-cyan-500 drop-shadow-md font-poppins">
+  <h2 className="text-4xl font-extrabold text-cyan-500 drop-shadow-md font-poppins">
     {t('Our Services')}
   </h2>
   <p className="mt-4 text-xl leading-7 text-gray-600 max-w-2xl mx-auto font-poppins px-4">
@@ -104,10 +104,10 @@ const Home = () => {
       key={index}
       href={service.link}
       rel="noopener noreferrer"
-      className="relative flex flex-col items-center justify-center bg-white p-8 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105 border border-cyan-500"
+      className="relative flex flex-col items-center justify-center bg-white p-8 rounded-3xl transition-transform duration-300 transform hover:scale-105 border border-cyan-500"
       data-aos="zoom-in-up"
     >
-      <div className="flex items-center justify-center h-16 w-16 rounded-full bg-cyan-500 text-white mb-4 shadow-lg">
+      <div className="flex items-center justify-center h-16 w-16 rounded-full bg-cyan-500 text-white mb-4">
         {service.icon}
       </div>
       <h3 className="text-2xl font-semibold text-cyan-500 mb-2 font-poppins text-center">
@@ -122,48 +122,50 @@ const Home = () => {
     </a>
   ))}
 </div>
+
 </div>
 </section>
-<section id="why-choose-us" className="py-6" data-aos="fade-up">
+<section id="why-choose-us" className="py-6 bg-cyan-50" data-aos="fade-up">
   <div className="w-full">
      <div className="text-center w-full">
   <h2 className="text-3xl font-bold text-cyan-500 drop-shadow-md font-poppins">
     {t('what_makes_unique')}
   </h2>
-  <p className="mt-4 text-xl leading-7 text-gray-600 max-w-2xl mx-auto font-poppins px-4">
+  <p className="mt-4 text-xl leading-7 text-gray-800 max-w-2xl mx-auto font-poppins px-4">
     {t('what_makes_unique_description')}
   </p>
 </div>
-    <div className="mt-12 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      {[
-        {
-          title: t('Experience'),
-          description: t('describe_exprience'),
-        },
-        {
-          title: t('Quality'),
-          description: t('Quality_Desription'),
-        },
-        {
-          title: t('Support'),
-          description: t('customer_support'),
-        },
-      ].map((benefit, index) => (
-        <div
-          key={index}
-          className="relative flex flex-col items-center justify-center bg-white p-8 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105 border border-cyan-500"
-          data-aos="zoom-in-up"
-        >
-          <h3 className="text-2xl font-semibold text-cyan-500 mb-2 font-poppins text-center">
-            {benefit.title}
-          </h3>
-          <p className="text-lg text-gray-600 font-poppins text-center">
-            {benefit.description}
-          </p>
-          <div className="absolute inset-0 rounded-xl bg-gradient-to-b from-transparent to-blue-100 opacity-0 hover:opacity-20 transition-opacity duration-300"></div>
-        </div>
-      ))}
+<div className="mt-12 grid grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+  {[
+    {
+      title: t('Experience'),
+      description: t('describe_exprience'),
+    },
+    {
+      title: t('Quality'),
+      description: t('Quality_Desription'),
+    },
+    {
+      title: t('Support'),
+      description: t('customer_support'),
+    },
+  ].map((benefit, index) => (
+    <div
+      key={index}
+      className="relative flex flex-col items-center justify-center bg-white p-8 rounded-xl transition-transform duration-300 transform hover:scale-x-150 border border-cyan-500"
+      data-aos="zoom-in-up"
+    >
+      <h3 className="text-2xl font-semibold text-cyan-500 mb-2 font-poppins text-center">
+        {benefit.title}
+      </h3>
+      <p className="text-lg text-gray-600 font-poppins text-center">
+        {benefit.description}
+      </p>
+      <div className="absolute inset-0 rounded-xl bg-gradient-to-b from-transparent to-blue-100 opacity-0 hover:opacity-20 transition-opacity duration-300"></div>
     </div>
+  ))}
+</div>
+
   </div>
 </section>
 <Footer data-aos="fade-up" />
